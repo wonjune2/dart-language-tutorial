d95040d5929092014184d2811494b9238e3386ed
Joon <zzzdnjswns@naver.com>
2023-02-16 00:50:41 +0900

    strings

--- built-in-types/strings.dart
+++ built-in-types/strings.dart
@@ -1,11 +1,28 @@
-var s = 'string interpolation';
-
 void main(List<String> args) {
-  assert('Dart has $s, which is very handy.' ==
-      'Dart has string interpolation, '
-          'which is very handy.');
-  assert('That deserves all caps. '
-          '${s.toUpperCase()} is very handy!' ==
-      'That deserves all caps. '
-          'STRING INTERPOLATION is very handy!');
+  var s1 = 'String '
+      'concatenation'
+      " works even over line breaks.";
+  assert(s1 ==
+      'String concatenation works even over '
+          'line breaks.');
+
+  var s2 = """This is also a
+    multi-line string.""";
+
+  var s = r'In a raw string, not even \n gets special treatment.';
+  print(s);
+
+// These work in a const string.
+  const aConstNum = 0;
+  const aConstBool = true;
+  const aConstString = 'a constant string';
+
+// These do NOT work in a const string.
+  const aNum = 0;
+  const aBool = true;
+  const aString = 'a string';
+  const aConstList = [1, 2, 3];
+
+  const validConstString = '$aConstNum $aConstBool $aConstString';
+  // const invalidConstString = '$aNum $aBool $aString $aConstList';
 }
